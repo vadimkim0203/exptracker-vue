a12afa6202c389a037b7b624ddd2debff380fe1e
Vadim Kim <vkim0203@gmail.com>
2024-11-28 11:23:03 +0900

    transaction amount logic added

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,27 @@
-<template>My app</template>
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <Transactions />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import Transactions from "./components/Transactions.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+    Transactions,
+    AddTransaction,
+  },
+}
+</script>
---
+++ src/components/Transactions.vue
@@ -0,0 +1,31 @@
+<template>
+  <h3>History</h3>
+  <li v-for="transaction in transactions" v-bind:key="transaction.id"
+    v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'">
+    {{ transaction.text }} <span>-${{ transaction.amount }}</span><button class="delete-btn">x</button>
+  </li>
+  <!-- <ul id="list" class="list">
+    <li class="minus">
+      Cash <span>-$400</span><button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+      Monthly salary <span>$2000</span><button class="delete-btn">x</button>
+    </li> 
+  </ul> -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "lunch", amount: -10.00 },
+        { id: 2, text: "daiso", amount: -12.89 },
+        { id: 3, text: "lotto", amount: -3.55 },
+        { id: 4, text: "coupang", amount: -18.29 },
+
+      ],
+    }
+  }
+}
+</script>
